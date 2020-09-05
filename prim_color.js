35319b9dacf2cebe53827382653e80e8edb1b116
tofitech <57547195+tofitech@users.noreply.github.com>
2020-09-05 16:58:21 +0800

    Update prim_color.js
    fixed the color red button
    => issue resolved.

--- prim_color.js
+++ prim_color.js
@@ -1,17 +1,18 @@
 
+
 const btn_red = document.querySelector('#btn_red');
 const btn_yellow = document.querySelector('#btn_yellow');
 const btn_blue = document.querySelector('#btn_blue');
 const btn_orange = document.querySelector('#btn_orange');
 const btn_green = document.querySelector('#btn_green');
 const btn_violet = document.querySelector('#btn_violet');
-const pink = document.querySelector('#q');
+
 
 //The body tag..
 const html_body = document.querySelector('body');
 
 //color red
-btn_red.addEventListener('click', () => html_body.style.backgroundColor = red);
+btn_red.addEventListener('click', () => html_body.style.backgroundColor = 'red');
 
 //color yellow
 btn_yellow.addEventListener('click', () => html_body.style.backgroundColor = 'yellow');
